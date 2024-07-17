97618a8fff5053c3538d2bf13879294bfbbad253
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 17:25:20 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Hello from "./Hello";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: true
+        };
+    }
+
+    delete = () => {
+        this.setState({display: false});
+    };
+
+    render() {
+        let comp;
+        if (this.state.display) {
+            comp = <Hello/>;
+        }
+        return (
+            <div style={{textAlign: 'center'}}>
+                {comp}
+                <button onClick={this.delete}>
+                    Delete the component
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
